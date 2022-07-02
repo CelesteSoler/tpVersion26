e46970efb67201834ee279ca5a0816472b41bff1
Celeste Soler <celestesoler33@gmail.com>
2022-07-02 11:39:41 -0300

    devuelve un ciclo

--- DFS_Ciclo.java
+++ DFS_Ciclo.java
@@ -9,57 +9,50 @@ import java.util.HashMap;
 
 	public class DFS_Ciclo {
 		private Grafo <String> grafo;
-		private HashMap<String,String>colores;//la idea es no meterle el color al vertice, guardo los colores de cada de vertice por fuera,
-		private Grafo<String> grafoCiclo;
+		private HashMap<String,String>colores;
+		private ArrayList<String> verticesAfines;
 		
 		public DFS_Ciclo (Grafo <String> grafo) {
 			this.grafo = grafo;
 			this.colores = new HashMap<>();
-			
 		}
 		
-		public  ArrayList<String>   hayCiclo(String generoA) {//ejercicio 3
+		public  ArrayList<String> hayCiclo(String generoA) {//ejercicio 3
 			
 			Iterator<String> it = this.grafo.obtenerVertices();
 			while (it.hasNext()) {
 				String verticeId = it.next();
 				colores.put(verticeId, "blanco");
 			}
-			
-			ArrayList< ArrayList<String> >  caminos  = dfs_visit(generoA,generoA);
-		
-			return caminos;	
+			ArrayList<String> caminoParcial = new ArrayList<String>();
+			verticesAfines  = dfs_visit(generoA,generoA,caminoParcial);
+			//generar el grafo con la lista de generos de los ciclos
+			//va a haber repetidos, controlar
+			//crear el grafo y los arcos
+			return verticesAfines;	
 		}
 		
-		public  ArrayList<String>   dfs_visit(String vertice, String generoA){//le paso tambien por parametro un camino parcial) {
-			 ArrayList<String>   todosLosCiclos = new ArrayList<>();
+		public  ArrayList<String>   dfs_visit(String vertice, String generoA,ArrayList<String> caminoParcial ){
 			colores.put(vertice, "amarillo");
+			Iterator<String> it = this.grafo.obtenerAdyacentes(vertice);
 			
-			Iterator<String> it = this.grafo.obtenerAdyacentes(vertice);//tengo que recorrer tods los adyacentes
-			//boolean encontre = false;
-			
-			while(it.hasNext()) {//mientras tenga adyacente
-				String adyacente = it.next();//obtengo el id del vertice adyacente
+			while(it.hasNext()) {
+				String adyacente = it.next();
 				
 				if(colores.get(adyacente).equals("blanco")) { 
-					
-					 ArrayList<String>  todosLosSubCaminos =dfs_visit(adyacente,generoA);
-					
-					for(ArrayList<String> SubCamino: todosLosSubCaminos) {
-						SubCamino.add(0,vertice);	
-						todosLosCiclos.add(SubCamino);
-					}
+					caminoParcial=dfs_visit(adyacente,generoA,caminoParcial);
+					 caminoParcial.add(0,vertice);	
 				}
-				else if (colores.get(adyacente).equals("amarillo")&& adyacente.equals(generoA)) {//si lo visite y esta en amariillo, quiere decir que ya fue visitado, encontes lo pinto en negro
-					//retorno el camino parcial
-					//encontre = true;//si hay un ciclo donde lo encontre corte, lo retorno
-					//System.out.println("encontre");
-					//return todos 
+				
+				else if (colores.get(adyacente).equals("amarillo")&& adyacente.equals(generoA)) { 
+					System.out.println("lo encontre");
+					//caminoParcial.add((caminoParcial.size()-1),adyacente);
+					this.verticesAfines.addAll(caminoParcial);
+					return caminoParcial; 
 				}
 			}
-			//colores.put(vertice, "negro");
 			
-			return todosLosCiclos;
+			return caminoParcial;
 			
 		}
 		
